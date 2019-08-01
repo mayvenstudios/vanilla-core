9eeea128bb33358174888815b0b761c7985b3c85
Ivan Kulikov <ikoolik@gmail.com>
2019-08-01 18:28:12 +0300

    do not fire updated when post is moved to trash + file deleted when post is trashed and deleted

--- Concerns/HandlesEvents.php
+++ Concerns/HandlesEvents.php
@@ -2,20 +2,39 @@
 
 namespace Vanilla\Concerns;
 
-trait HandlesEvents {
+trait HandlesEvents
+{
     public function registerEvents()
     {
-        add_action('save_post', [$this, 'handlePostUpdated'], 10, 3);
+        add_action('post_updated', [$this, 'handlePostUpdated'], 10, 3);
+        add_action('delete_post', [$this, 'handlePostDeleted']);
+        add_action('wp_trash_post', [$this, 'handlePostDeleted']);
+
+
     }
 
     public function handlePostUpdated($id, $post, $update)
     {
+        if($post->post_status === 'trash') return;
+
         $postType = get_post_type_object($post->post_type);
         $class = isset($postType->className) ? $postType->className : null;
 
-        if($class) {
+        if ($class) {
             $object = new $class($post);
-            if(method_exists($object, 'updated')) $object->updated();
+            if (method_exists($object, 'updated')) $object->updated();
+        }
+    }
+
+    public function handlePostDeleted($id)
+    {
+        $post = get_post($id);
+        $postType = get_post_type_object($post->post_type);
+        $class = isset($postType->className) ? $postType->className : null;
+
+        if ($class) {
+            $object = new $class($post);
+            if (method_exists($object, 'deleted')) $object->deleted();
         }
     }
 }
